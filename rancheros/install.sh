664584f6ed861e331206cf3a072bd2c5541139d7
Felipe Crochik <github@b2-4ac.com>
2019-03-01 13:51:18 -0500

    Update install.sh

--- rancheros/install.sh
+++ rancheros/install.sh
@@ -1,4 +1,4 @@
 # bash/sh
 docker pull portainer/portainer
-docker save -o /usr/lib/rancher/preload/docker/portainer.tar portainer/portainer
+docker save -o /var/lib/rancher/preload/docker/portainer.tar portainer/portainer
 ros install -c https://www.crochik.com/tldr/rancheros/cloud-config.yml -d /dev/sda --append "rancher.autologin=tty1"
