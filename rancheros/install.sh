d07a8f6d4868124149bde8e0dffdb79c7ab88e06
Felipe Crochik <github@b2-4ac.com>
2019-03-01 12:16:25 -0500

    Update install.sh

--- rancheros/install.sh
+++ rancheros/install.sh
@@ -1,1 +1,1 @@
-ros install -c cloud-config.yml -d /dev/sda --append "rancher.autologin=tty1"
+ros install -c https://www.crochik.com/tldr/rancheros/cloud-config.yml -d /dev/sda --append "rancher.autologin=tty1"
