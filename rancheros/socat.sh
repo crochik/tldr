d888deeb4a7a06a55b00ac34cab6ff0f5e43b377
Felipe Crochik <github@b2-4ac.com>
2019-03-05 15:37:55 -0500

    Create socat.sh

---
+++ rancheros/socat.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+docker run -d --restart=always \
+    -p 127.0.0.1:2376:2375 \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    alpine/socat \
+    tcp-listen:2375,fork,reuseaddr unix-connect:/var/run/docker.sock
