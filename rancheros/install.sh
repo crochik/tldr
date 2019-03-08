6449994623c2e05a579d993c63b77644680daf79
Felipe Crochik <felipe@crochik.com>
2019-03-08 11:14:22 -0500

    move docker tmp folder

--- rancheros/install.sh
+++ rancheros/install.sh
@@ -5,8 +5,15 @@ mkdir sda1
 sudo mount -t ext4 /dev/sda1 sda1
 sudo mkdir -p sda1/var/lib/rancher/preload/docker
 
+# move docker tmp folder to sda1
+sudo mkdir -p sda1/var/lib/tmp
+rmdir /var/lib/docker/tmp
+ln -s /home/rancher/sda1/var/lib/tmp /var/lib/docker/tmp
+
 docker pull guacamole/guacd:latest
 docker pull crochik/abacate:latest
 docker pull portainer/portainer
 
 docker save -o sda1/var/lib/rancher/preload/docker/docker-images.tar portainer/portainer crochik/abacate:latest guacamole/guacd:latest
+
+#reboot
