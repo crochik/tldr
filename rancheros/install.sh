a2cc5134d271307c492d92e75e354b7743cdacf6
Felipe Crochik <felipe@crochik.com>
2019-03-08 10:19:32 -0500

    save docker images

--- rancheros/install.sh
+++ rancheros/install.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+ros install -c https://raw.githubusercontent.com/crochik/tldr/master/rancheros/cloud-config.yml -d /dev/sda --append "rancher.autologin=tty1"
+
+mkdir sda1
+sudo mount -t ext4 /dev/sda1 sda1
+sudo mkdir -p sda1/var/lib/rancher/preload/docker
+
 docker pull guacamole/guacd:latest
 docker pull crochik/abacate:latest
 docker pull portainer/portainer
 
-ros install -c https://raw.githubusercontent.com/crochik/tldr/master/rancheros/cloud-config.yml -d /dev/sda --append "rancher.autologin=tty1"
-
-docker save -o ~/docker-images.tar portainer/portainer crochik/abacate:latest guacamole/guacd:latest
+docker save -o sda1/var/lib/rancher/preload/docker/docker-images.tar portainer/portainer crochik/abacate:latest guacamole/guacd:latest
